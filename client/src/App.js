4011f344e04ad5409b71701db913f784b701857f
Adi Pillai <76452932+Adi9103@users.noreply.github.com>
2023-12-06 23:19:04 -0600

    updated styling?

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Order from './components/Order';
@@ -8,29 +9,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">MailOrderMeals</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/order">Order</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/profile">Profile</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/order" element={<Order />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </Router>
+		<nav className="navbar navbar-expand-lg navbar-light navbar-custom">
+			<div className="container-fluid">
+					<Link className="navbar-brand" to="/">MailOrderMeals</Link>
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<Link className="nav-link" to="/order">Order</Link>
+							</li>
+						<li className="nav-item">
+							<Link className="nav-link" to="/profile">Profile</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+      	<div className="container mt-4">
+			<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/order" element={<Order />} />
+			<Route path="/profile" element={<Profile />} />
+			</Routes>
+		</div>
+	</Router>
   );
 };
 
