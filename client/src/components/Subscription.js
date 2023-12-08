1df37a37caac9c114b60013b704c842adbce931f
Adi Pillai <76452932+Adi9103@users.noreply.github.com>
2023-12-08 10:17:14 -0600

    update profile, subscription, very close to finishing

--- client/src/components/Subscription.js
+++ client/src/components/Subscription.js
@@ -1,75 +1,112 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Subscription = () => {  
-  const [product, setProduct] = useState(''); 
-  const [frequency, setFrequency] = useState('');
-  const [deliveryAddress, setDeliveryAddress] = useState('');
-  const [deliveryDay, setDeliveryDay] = useState('');
-  const userId = localStorage.getItem('userId');
+const Subscription = () => {
+    const [products, setProducts] = useState([]);
+    const [size, setSize] = useState('');
+    const [product, setProduct] = useState('');
+    const [frequency, setFrequency] = useState('');
+    const [deliveryAddress, setDeliveryAddress] = useState('');
+    const [deliveryDay, setDeliveryDay] = useState('');
+    const userId = localStorage.getItem('userId');
 
-  const submitSubscription = () => {
-    const subscriptionData = {
-      product,
-      frequency,
-      deliveryAddress,
-      deliveryDay,
+    useEffect(() => {
+        fetch('http://localhost:4000/products')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch(error => console.error('Error fetching products:', error));
+    }, []);
+
+    const submitSubscription = () => {
+        const subscriptionData = {
+            product,
+            frequency,
+            deliveryAddress,
+            deliveryDay,
+        };
+        console.log("Sending subscription data:", subscriptionData);
+
+        fetch(`http://localhost:4000/subscriptions/${userId}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(subscriptionData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Subscription creation failed');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Subscription created:', data);
+            })
+            .catch(error => {
+                console.error('Error during subscription creation:', error);
+                if (error.message) {
+                    console.error('Error message:', error.message);
+                }
+            });
     };
 
-    fetch(`http://localhost:4000/subscriptions/${userId}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(subscriptionData)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Subscription creation failed');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Subscription created:', data);
-    })
-    .catch(error => {
-        console.error('Error during subscription creation:', error);
-        if (error.message) {
-            console.error('Error message:', error.message);
-        }
-    });
-  };
+    return (
+        <div>
+            {/* <input
+type="text"
+className="form-control mb-2"
+placeholder="Product"
+value={product}
+onChange={(e) => setProduct(e.target.value)}
+/> */}
+            <select
+                className="form-control mb-2"
+                value={product}
+                onChange={(e) => setProduct(e.target.value)}
+            >
+                <option value="">Select a Product</option>
+                {Array.from(new Set(products.map((prod) => prod.name))).map((productName) => (
+                    <option key={productName} value={productName}>
+                        {productName}
+                    </option>
+                ))}
+            </select>
+            <select
+                className="form-control mb-2"
+                value={size}
+                onChange={(e) => setSize(e.target.value)}
+            >
+                <option value="">Select a Size</option>
+                {product && products
+                    .filter((prod) => prod.name === product)
+                    .map((prod) => (
+                        <option key={prod._id} value={prod.size}>
+                            {prod.size}
+                        </option>
+                    ))}
+            </select>
 
-  return (
-    <div>
-      <input
-        type="text"
-        className="form-control mb-2"
-        placeholder="Product"
-        value={product}
-        onChange={(e) => setProduct(e.target.value)}
-      />
-      <input
-        type="text"
-        className="form-control mb-2"
-        placeholder="Frequency"
-        value={frequency}
-        onChange={(e) => setFrequency(e.target.value)}
-      />
-      <input
-        type="text"
-        className="form-control mb-2"
-        placeholder="Delivery Address"
-        value={deliveryAddress}
-        onChange={(e) => setDeliveryAddress(e.target.value)}
-      />
-      <input
-        type="text"
-        className="form-control mb-2"
-        placeholder="Delivery Day"
-        value={deliveryDay}
-        onChange={(e) => setDeliveryDay(e.target.value)}
-      />
-      <button className="btn btn-primary" onClick={submitSubscription}>Submit Subscription</button>
-    </div>
-  );
+            <input
+                type="text"
+                className="form-control mb-2"
+                placeholder="Frequency"
+                value={frequency}
+                onChange={(e) => setFrequency(e.target.value)}
+            />
+            <input
+                type="text"
+                className="form-control mb-2"
+                placeholder="Delivery Address"
+                value={deliveryAddress}
+                onChange={(e) => setDeliveryAddress(e.target.value)}
+            />
+            <input
+                type="text"
+                className="form-control mb-2"
+                placeholder="Delivery Day"
+                value={deliveryDay}
+                onChange={(e) => setDeliveryDay(e.target.value)}
+            />
+            <button className="btn btn-primary" onClick={submitSubscription}>Submit Subscription</button>
+        </div>
+    );
 };
 
 export default Subscription;
